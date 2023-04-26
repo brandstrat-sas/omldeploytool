b46cac69e75e4e2dbf2e56414ba668092dda343c
fpignataro <fabian.pignataro84@gmail.com>
2023-04-26 06:47:40 -0300

    docker-compose AIO

--- docker-compose/user-data.sh
+++ docker-compose/user-data.sh
@@ -9,7 +9,7 @@ cp env .env
 
 sed -i -e "s/S3_ENDPOINT=https://localhost/S3_ENDPOINT=https://${host_ipaddr}/g" .env
 
-docker-compose up -d
+docker-compose -f docker-compose_aio.yml up -d
 
 until curl -sk --head  --request GET https://localhost |grep "302" > /dev/null; do echo "Environment still initializing , sleeping 10 seconds"; sleep 10; done; echo "Environment is up"
 bash -x oml_manage --reset_pass
