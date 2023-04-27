283122f2d34cfecac1d8f32846f0c8359ad71379
fpignataro <fabian.pignataro84@gmail.com>
2023-04-27 16:37:14 -0300

    CICD digital

--- docker-compose/user-data.sh
+++ docker-compose/user-data.sh
@@ -31,12 +31,14 @@ sed -i "s/^REDIS_HOSTNAME=redis/REDIS_HOSTNAME=$PRIVATE_IPV4/g" .env
 sed -i "s/RTPENGINE_HOSTNAME=rtpengine/RTPENGINE_HOSTNAME=$PRIVATE_IPV4/g" .env
 sed -i "s/minio:9000/$PRIVATE_IPV4:9000/g" .env
 sed -i "s/redis:6379/$PRIVATE_IPV4:6379/g" .env
-sed -i "s/S3_ENDPOINT=https://localhost/S3_ENDPOINT=https://$PUBLIC_IPV4/g" .env
-sed -i "s/S3_ENDPOINT_MINIO=http://minio:9000/S3_ENDPOINT_MINIO=http://localhost:9000/g" .env
+sed -i "s%\S3_ENDPOINT=https://localhost%S3_ENDPOINT=https//$PUBLIC_IPV4%g" .env
+sed -i "s%\S3_ENDPOINT_MINIO=http://minio:9000%S3_ENDPOINT_MINIO=http://localhost:9000%g" .env
 
-docker-compose -f docker-compose_aio.yml up -d
+/usr/libexec/docker/cli-plugins/docker-compose -f docker-compose_aio.yml up -d
 
-until curl -sk --head  --request GET https://localhost |grep "302" > /dev/null; do echo "Environment still initializing , sleeping 10 seconds"; sleep 10; done; echo "Environment is up"
-bash -x oml_manage --reset_pass
+ln -s /root/omldeploytool/docker-compose/oml_manage /usr/local/bin
+
+#until curl -sk --head  --request GET https://localhost |grep "302" > /dev/null; do echo "Environment still initializing , sleeping 10 seconds"; sleep 10; done; echo "Environment is up"
+#bash -x oml_manage --reset_pass
 
 # bash -x oml_manage --init_env
