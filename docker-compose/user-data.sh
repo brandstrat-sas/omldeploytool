680f855e2c17f7ea92a9d79df5dfb785efa3113d
fpignataro <fabian.pignataro84@gmail.com>
2023-05-04 12:18:39 -0300

    minio aio fix

--- docker-compose/user-data.sh
+++ docker-compose/user-data.sh
@@ -16,7 +16,7 @@ git checkout oml-289-dev-aio-compose-observability
 cp env .env
 
 sed -i "s/ENV=devenv/ENV=cloud/g" .env
-sed -i "s/CALLREC_DEVICE=s3-minio/CALLREC_DEVICE=s3/g" .env
+#sed -i "s/CALLREC_DEVICE=s3-minio/CALLREC_DEVICE=s3/g" .env
 sed -i "s/DJANGO_HOSTNAME=app/DJANGO_HOSTNAME=$PRIVATE_IPV4/g" .env
 sed -i "s/DAPHNE_HOSTNAME=channels/DAPHNE_HOSTNAME=$PRIVATE_IPV4/g" .env
 sed -i "s/ASTERISK_HOSTNAME=acd/ASTERISK_HOSTNAME=$PRIVATE_IPV4/g" .env
@@ -33,7 +33,8 @@ sed -i "s%\S3_ENDPOINT_MINIO=http://minio:9000%S3_ENDPOINT_MINIO=http://localhos
 
 /usr/libexec/docker/cli-plugins/docker-compose -f docker-compose_aio.yml up -d
 
-ln -s /root/omldeploytool/docker-compose/oml_manage /usr/local/bin
+ln -s /root/omldeploytool/docker-compose/oml_manage /usr/local/bin/
+ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/
 
 #until curl -sk --head  --request GET https://localhost |grep "302" > /dev/null; do echo "Environment still initializing , sleeping 10 seconds"; sleep 10; done; echo "Environment is up"
 #bash -x oml_manage --reset_pass
