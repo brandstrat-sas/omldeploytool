c1eba29376b6b14e8e3c8ac5bed8dc5b8a56910f
fpignataro <fabian.pignataro84@gmail.com>
2023-05-15 12:50:52 -0300

    deploy ansible compose

--- docker-swarm/components/asterisk/templates/asterisk_transition.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "$(date): transition to master" >> /var/log/keepalive_transition.log
-source /etc/omnileads-asterisk.env && source /opt/omnileads/asterisk/virtualenv/bin/activate && /opt/omnileads/asterisk/virtualenv/bin/python3 /opt/omnileads/asterisk/virtualenv/asterisk_transition.py >> /var/log/keepalive_transition.log 2>&1
-
-exit 0
