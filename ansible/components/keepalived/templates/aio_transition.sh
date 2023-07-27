023504740be9cfcd3b8fdfe89c7e9f30be2166de
fpignataro <fabian.pignataro84@gmail.com>
2023-07-27 14:06:47 -0300

    transition logs

--- ansible/components/keepalived/templates/aio_transition.sh
+++ ansible/components/keepalived/templates/aio_transition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$(date): transition to master" >> /var/log/keepalive_transition.log
+echo "$(date): transition to master, exec oml_cluster_transition.sh" >> /var/log/keepalive_transition.log
 
 /usr/bin/podman exec -it oml-asterisk-server asterisk -rx 'pjsip reload'
 sleep 3
