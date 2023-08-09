c0f4fdf88a568ad58461df52c1e3adc02d6c8346
fpignataro <fabian.pignataro84@gmail.com>
2023-08-09 17:38:56 -0300

    HA asterisk AMI socket VIP

--- ansible/components/keepalived/templates/aio_transition.sh
+++ ansible/components/keepalived/templates/aio_transition.sh
@@ -3,6 +3,7 @@
 echo "$(date): transition to master, exec oml_cluster_transition.sh" >> /var/log/keepalive_transition.log
 
 /usr/bin/podman exec -it oml-asterisk-server asterisk -rx 'pjsip reload'
+/usr/bin/podman exec -it oml-asterisk-server asterisk -rx 'manager reload'
 sleep 3
 /usr/bin/podman exec -it oml-asterisk-server python3 /opt/asterisk/virtualenv/scripts/asterisk_transition.py
 sleep 3
