46ae35787f2c8104278462e79f6d0c10d67b71ec
fpignataro <fabian.pignataro84@gmail.com>
2023-08-11 12:29:02 -0300

    asterisk restart on transition

--- ansible/components/keepalived/templates/aio_transition.sh
+++ ansible/components/keepalived/templates/aio_transition.sh
@@ -2,9 +2,8 @@
 
 echo "$(date): transition to master, exec oml_cluster_transition.sh" >> /var/log/keepalive_transition.log
 
-/usr/bin/podman exec -it oml-asterisk-server asterisk -rx 'pjsip reload'
-/usr/bin/podman exec -it oml-asterisk-server asterisk -rx 'manager reload'
-sleep 3
-/usr/bin/podman exec -it oml-asterisk-server python3 /opt/asterisk/virtualenv/scripts/asterisk_transition.py
-sleep 3
+systemctl restart asterisk
+#sleep 3
+#/usr/bin/podman exec -it oml-asterisk-server python3 /opt/asterisk/scripts/asterisk_transition.py
+sleep 30
 /usr/local/bin/oml_manage --regenerar_asterisk
