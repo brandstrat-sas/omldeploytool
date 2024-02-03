a59b4581935aa13c459d2689f9686896d45fa042
fpignataro <fabian.pignataro84@gmail.com>
2024-02-03 11:44:57 -0300

    transition HA regenerar luego restart, django commmands

--- ansible/components/keepalived/templates/aio_transition.sh
+++ ansible/components/keepalived/templates/aio_transition.sh
@@ -2,8 +2,6 @@
 
 echo "$(date): transition to master, exec oml_cluster_transition.sh" >> /var/log/keepalive_transition.log
 
-systemctl restart asterisk
-#sleep 3
-#/usr/bin/podman exec -it oml-asterisk-server python3 /opt/asterisk/scripts/asterisk_transition.py
-sleep 30
 /usr/local/bin/oml_manage --regenerar_asterisk
+sleep 10
+systemctl restart asterisk
